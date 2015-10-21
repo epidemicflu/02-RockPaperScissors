12d06ac8832c6a235fca35f878655f5b8cb03321
epidemicflu <hcedillo2018@bklawtech.com>
2015-10-21 21:25:16 +0000

    finished assignment.still needs some styling

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,17 +4,41 @@ var aiPoint = 0;
 // This function returns the selection of the computer
 function getAISelection() {
     //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    var random = Math.random();
+    if (random < 1/3){
+        return 'rock';
+    }
+    if (random < 2/3){
+        return 'scissors';
+    }
+        return 'paper';
+    
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
     //TODO: pick the correct winner: user or ai
     //TODO: Add one point for the winner
+    if(userValue == aiValue){
+        return 'draw';
+    }
+    // && and
+    // || or
+    if(userValue === 'rock' && aiValue === 'scissors' ||
+       userValue === 'paper' && aiValue === 'rock' ||
+       userValue === 'scissors' && aiValue === 'paper'){
+        userPoint++
+        return 'user'
+    }
+   aiPoint += 1;
+   return 'ai'
+    
 }
 
 // This function sets the scoreboard with the correct points
 function setScore() {
-
+  $('#userPoint').text(userPoint);
+  $('#aiPoint').text(aiPoint);
 }
 
 // This function captures the click and picks the winner
